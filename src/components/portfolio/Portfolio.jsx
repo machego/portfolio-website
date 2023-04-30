c5f8d6b5ac918c5d4dd8f6580ddfac29ae4bbfeb
machego <114348981+machego@users.noreply.github.com>
2023-04-30 22:38:14 +0200

    first version

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import './portfolio.scss'
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+      <div className='portfolio' id='portfolio'>
+          
+      </div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import './topbar.scss'
+
+const Topbar = () => {
+  return (
+      <div className='topbar'>
+        <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro'>assd</a>
+        </div>
+        <div className='right'></div>
+        </div>
+      </div>
+  )
+}
+
+export default Topbar
