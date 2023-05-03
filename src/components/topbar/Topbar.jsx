15b4600295c9905280419fe85dca8e9b88b06d19
machego <114348981+machego@users.noreply.github.com>
2023-05-03 17:57:33 +0200

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import './topbar.scss'
 import {Person, Mail} from '@material-ui/icons'
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-      <div className='topbar active'>
+    <div className={ 'topbar ' + (menuOpen && 'active') }>
         <div className='wrapper'>
           <div className='left'>
           <a href='#intro' className='logo'>genius.</a>
@@ -17,7 +17,7 @@ const Topbar = () => {
           </div>
           </div>
         <div className='right'>
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className='line1'></span>
             <span className='line2'></span>
             <span className='line3'></span>
